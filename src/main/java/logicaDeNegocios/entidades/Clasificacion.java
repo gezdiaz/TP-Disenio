e3edf23547fce4f974d516cf8e00ae61733e7136
EmanuelT18 <stomas@frsf.utn.edu.ar>
2018-11-05 16:38:13 -0300

    Agregamos Clasificacion y Grupo de Resolucion

--- src/main/java/logicaDeNegocios/entidades/Clasificacion.java
+++ src/main/java/logicaDeNegocios/entidades/Clasificacion.java
@@ -1,6 +1,94 @@
 package logicaDeNegocios.entidades;
 
+import java.time.LocalDate;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Clasificacion")
+
 public class Clasificacion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigo")
+	private Integer codigo;
+	
+	@Column(name = "nombre",nullable = false,length = 30)
+	private String nombre;
+	
+	@Column(name = "descripcion")
+	private String descripcion;
+	
+	@Column(name = "activa")
+	private Boolean activa;
+	
+	@Column(name = "fechaCreacion")
+	private LocalDate fechaCreacion;
+	
+	@ManyToMany()//Areglar no se como se hacen las manyToMany
+	private List<GrupoResolucion> gruposDeResolucion;
+	
+	
+	
+	
+	public Clasificacion() {
+		
+	}
+	
+	public Clasificacion(String nombre, String descripcion,ArrayList<GrupoResolucion> gruposDeResolucion) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.gruposDeResolucion = gruposDeResolucion;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Boolean getActiva() {
+		return activa;
+	}
+
+	public void setActiva(Boolean activa) {
+		this.activa = activa;
+	}
+
+	public LocalDate getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDate fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public List<GrupoResolucion> getGruposDeResolucion() {
+		return gruposDeResolucion;
+	}
+
+	public void setGruposDeResolucion(List<GrupoResolucion> gruposDeResolucion) {
+		this.gruposDeResolucion = gruposDeResolucion;
+	}
 
 	
 }
--- src/main/java/logicaDeNegocios/entidades/GrupoResolucion.java
+++ src/main/java/logicaDeNegocios/entidades/GrupoResolucion.java
@@ -1,7 +1,45 @@
 package logicaDeNegocios.entidades;
 
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Grupo_de_Resolucion")
+
 public class GrupoResolucion {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idGR")
+	private String codigo;
+	
+	@Column(name = "nombre",nullable = false,length = 50)
+	private String nombre;
+	
+	@OneToMany(fetch = FetchType.EAGER,mappedBy = "grupoResolucion")
+	private List<Usuario> usuarios;
+	
+	@OneToMany(fetch = FetchType.LAZY,mappedBy = "grupoResolucion")
+	private List<Intervencion> intervenciones;
+	
+	@ManyToMany()
+	private List<Clasificacion> clasificaciones;
+
+	public GrupoResolucion() {
+
+	}
+	
+	public GrupoResolucion(String codigo,String nombre,ArrayList<Usuario> usuarios) {
+		
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.usuarios = usuarios;
+		
+	}
+	
+	
+	
+	
 	
 	
 }
