ac774d59659a06fb312fc66861f2c8cf576426a8
Gaston <gastonezequieldiaz@outlook.com>
2018-11-23 18:18:38 -0300

    Eureka!!
    Conseguí que la ventana se achique al tamaño de cada pantalla :D

--- src/main/java/interfaz/base/VentanaBase.java
+++ src/main/java/interfaz/base/VentanaBase.java
@@ -36,7 +36,6 @@ public class VentanaBase extends JFrame {
 		panel.add(this.subPanel, "Inicio");
 		cartas.show(panel, "Inicio");
 		
-		
 
 		contentPane.add(barraUsuario, BorderLayout.NORTH);
 		contentPane.add(panel, BorderLayout.CENTER);
@@ -51,17 +50,43 @@ public class VentanaBase extends JFrame {
 	}
 
 	public void cambiarPanel(JPanel nuevoPanel) {
+		int diffH = (int)this.getSize().getHeight()-(int)subPanel.getSize().getHeight();
+		int diffW = (int)this.getSize().getWidth()-(int)subPanel.getSize().getWidth();
+		System.out.println("Panel anterior: "+subPanel.getSize());
 		this.subPanel = nuevoPanel;
-		
+		System.out.println("Nuevo panel antes de agregarlo: "+subPanel.getSize());
+		System.out.println("Preferred sise del nuevo: "+subPanel.getPreferredSize());
 		panel.add(nuevoPanel, "nuevoPanel");
 
 		cartas.show(panel, "nuevoPanel");
 
+		this.setMaximumSize(new Dimension((int)subPanel.getPreferredSize().getWidth()+diffW, (int)subPanel.getPreferredSize().getHeight()+diffH));
+		
 		this.revalidate();
 		this.repaint();
 		this.pack();
 		this.setLocationRelativeTo(null);
+		System.out.println("Nuevo panel después de mostrarlo: "+subPanel.getSize());
 	}
 	
 
+	@Override
+    public void paint(Graphics g) {
+        Dimension d = getSize();
+        Dimension m = getMaximumSize();
+        boolean resize = d.width > m.width || d.height > m.height;
+        d.width = Math.min(m.width, d.width);
+        d.height = Math.min(m.height, d.height);
+
+        if (resize) {
+//            Point p = getLocation();
+            setVisible(false);
+            setSize(d);
+//            setLocation(p);
+            setVisible(true);
+            setLocationRelativeTo(null);
+        }
+        super.paint(g);
+    }
+	
 }
